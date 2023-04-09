8e59ff5e51bbcbfbe98f4ace3815e26fa249c26d
omkundu <omkundu156@gmail.com>
2023-04-09 10:44:10 +0530

    second-commit-upadted

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors")
 require("dotenv").config();
 
+app.use(express.json())
+app.use(cors({
+    origin : "*"
+}))
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (req,res) => {
